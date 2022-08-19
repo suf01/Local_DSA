ab9b71066dd190e291ade08c250669dce79555cb
sufiyan <90792596+suf01@users.noreply.github.com>
2022-08-20 00:22:09 +0530

    Update 01_knapsack.cpp

--- 01_knapsack.cpp
+++ 01_knapsack.cpp
@@ -39,6 +39,32 @@ int knapSack(int W, int wt[], int val[], int n) {
     return solve(W, wt, val, n);
 }
 
+//top-down approach
+int knapSack(int W, int wt[], int val[], int n){
+    
+    int dp[1001][1001];
+       
+    for(int i = 0; i < n+1; i++){
+        for(int j = 0; j < W+1; j++){
+            if(i > 0 && j > 0){
+                break;
+            }
+            dp[i][j] = 0;
+        }
+    }
+       
+    for(int i = 1; i < n+1; i++){
+        for(int j = 1; j < W+1; j++){
+            if(wt[i-1] <= j){
+                dp[i][j] = max(dp[i-1][j], val[i-1]+dp[i-1][j-wt[i-1]]);
+            }else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+       
+    return dp[n][W];
+}
 int main(){
 
 }
