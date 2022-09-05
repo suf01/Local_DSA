a23048b18dfd62b961a2c94442a3d01a49a71cd3
sufiyan <90792596+suf01@users.noreply.github.com>
2022-09-05 12:53:37 +0530

    Update kadanes.cpp

--- kadanes.cpp
+++ kadanes.cpp
@@ -1,23 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-maxsum(vector<int> v){
+int maxsum(vector<int> v){
     int n = v.size();
 
     int cur = 0;
-    int max = 0;
+    int maxi = INT_MIN;
 
     for(int i = 0; i < n; i++){
         cur += v[i];
 
         if(cur > max){
-            max = cur;
+            maxi = cur;
         }
         if(cur < 0){
             cur = 0;
         }
     }
     return max;
+}
+
+int smallestSumSubarray(vector<int>& a){
+    int mini = INT_MAX;
+    int curr = 0;
+      
+    for(int i = 0; i < a.size(); i++){
+        
+        if(curr > 0){
+            curr = a[i];
+        }else{
+            curr += a[i];
+        }
+          
+        if(curr < mini){
+            mini = curr;
+        }
+    }
+    
+    return mini;
 }
 
 
