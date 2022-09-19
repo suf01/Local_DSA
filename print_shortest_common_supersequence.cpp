bf342e3e85db8c7880da1b32fafd8194baeb33dd
sufiyan <90792596+suf01@users.noreply.github.com>
2022-09-19 18:09:06 +0530

    Create print_shortest_common_supersequence.cpp

---
+++ print_shortest_common_supersequence.cpp
@@ -0,0 +1,50 @@
+string shortestCommonSupersequence(string str1, string str2) {
+    int n = str1.size();
+    int m = str2.size();
+        
+    int dp[n+1][m+1];
+        
+    memset(dp, 0, sizeof(dp));
+        
+    for(int i = 1; i < n+1; i++){
+        for(int j = 1; j < m+1; j++){
+            if(str1[i-1] == str2[j-1]){
+                dp[i][j] = 1+dp[i-1][j-1];
+            }else{
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+        
+    int i = n;
+    int j = m;
+        
+    string ans = "";
+    while(i > 0 && j > 0){
+        if(str1[i-1] == str2[j-1]){
+            ans.push_back(str1[i-1]);
+            i--;
+            j--;
+        }else{
+            if(dp[i-1][j] > dp[i][j-1]){
+                ans.push_back(str1[i-1]);
+                i--;
+            }else{
+                ans.push_back(str2[j-1]);
+                j--;
+            }
+        }
+    }
+        
+    while(i > 0){
+        ans.push_back(str1[i-1]);
+        i--;
+    }
+    while(j > 0){
+        ans.push_back(str2[j-1]);
+        j--;
+    }
+        
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
