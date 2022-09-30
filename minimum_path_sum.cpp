3fda5161b7cf5be03de995a250c353929440bfa3
sufiyan <90792596+suf01@users.noreply.github.com>
2022-09-30 22:39:11 +0530

    Update minimum_path_sum.cpp

--- minimum_path_sum.cpp
+++ minimum_path_sum.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//recursion + memoziation
 int find_path(vector<vector<int>>& grid, int i, int j, vector<vector<int> > &dp){
     if(i == grid.size() || j == grid[0].size()){
         return 1000000;
@@ -29,4 +30,30 @@ int minPathSum(vector<vector<int>>& grid) {
     int j = 0;
 
     return find_path(grid, i, j, dp);
+}
+
+//top down dp
+int minPathSum(vector<vector<int>>& grid) {
+    int n = grid.size();
+    int m = grid[0].size();
+
+    int dp[n][m];
+    memset(dp, -1, sizeof(dp));
+
+    dp[0][0] = grid[0][0];
+
+    for(int i = 1; i < n; i++){
+        dp[i][0] = dp[i-1][0] + grid[i][0];
+    }
+    for(int j = 1; j < m; j++){
+        dp[0][j] = dp[0][j-1] + grid[0][j];
+    }
+
+    for(int i = 1; i < n; i++){
+        for(int j = 1; j < m; j++){
+            dp[i][j] = min(dp[i-1][j], dp[i][j-1])+grid[i][j];
+        }
+    }
+        
+    return dp[n-1][m-1];
 }
