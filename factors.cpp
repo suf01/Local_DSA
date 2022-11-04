3b9846513e307d37ffd9ca01feb9b79a10f2b8f8
sufiyan <90792596+suf01@users.noreply.github.com>
2022-11-04 14:10:00 +0530

    Create factors.cpp

---
+++ factors.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int N;
+	cin>>N;
+
+	int lim = sqrt(N);
+	for(int i = 1; i <= lim; i++){
+		if(N%i == 0){
+			cout<<i<<" ";
+			if(N/i != i){
+				cout<<N/i<<" ";
+			}
+		}
+	}
+  
+	cout<<endl<<endl;
+	
+  //to count factors of each number from 1 to N
+	vector<int> fact_cnt(N+1, 0);
+	for(int i = 1; i <= N; i++){
+		for(int j = i; j <= N; j = j+i){
+			fact_cnt[j]++;
+		}
+	}
+	
+  for(int i = 1; i <= N; i++){
+		cout<<fact_cnt[i]<<" ";
+	}
+}
