849991898bf14316eb43d97e1ae4303a490ea30e
sufiyan <90792596+suf01@users.noreply.github.com>
2022-11-04 14:11:19 +0530

    Update factors.cpp

--- factors.cpp
+++ factors.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main(){
 	int N;
 	cin>>N;
-
+	
 	int lim = sqrt(N);
+	//O(sqrt(N))
 	for(int i = 1; i <= lim; i++){
 		if(N%i == 0){
 			cout<<i<<" ";
@@ -19,6 +20,7 @@ int main(){
 	
   //to count factors of each number from 1 to N
 	vector<int> fact_cnt(N+1, 0);
+	//O(NlogN)
 	for(int i = 1; i <= N; i++){
 		for(int j = i; j <= N; j = j+i){
 			fact_cnt[j]++;
