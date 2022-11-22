70f36e461d7fb27df5ab71b89218e22dc86a54b2
sufiyan <90792596+suf01@users.noreply.github.com>
2022-11-22 20:49:04 +0530

    Create floyd_warshall.cpp

---
+++ floyd_warshall.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// T.C O(n^3) 
+// floyd warshall gives multisource shortest path
+// also detects negative cycle
+// if(matrix[i][i] < 0) graph contains negative cycle
+
+void shortest_distance(vector<vector<int>>&matrix){
+        
+    int n = matrix.size();
+        
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(matrix[i][j] == -1){
+                matrix[i][j] = 1e9;
+            }
+            if(i == j){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+        
+    for(int via = 0; via < n; via++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                matrix[i][j] = min(matrix[i][j], matrix[i][via]+matrix[via][j]);
+            }
+        }
+    }
+        
+        
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(matrix[i][j] == 1e9){
+                matrix[i][j] = -1;
+            }
+        }
+    }
+}
