cea29f00a3b89127404e63773bbe03a7d5ca646f
sufiyan <90792596+suf01@users.noreply.github.com>
2023-04-14 21:36:50 +0530

    Create count_palindromic_subsequences.cpp

---
+++ count_palindromic_subsequences.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countPalindromes(string s) {
+    int n = s.size();
+
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    int ans = 0;
+    for(int g = 0; g < n; g++){
+        for(int i = 0, j = g; j < n; i++, j++){
+            if(g == 0){
+                dp[i][j] = 1;
+            }else if(g == 1){
+                if(s[i] == s[j]){
+                    dp[i][j] = 3;
+                }else{
+                    dp[i][j] = 2;
+                }
+            }else{
+                if(s[i] == s[j]){
+                    dp[i][j] = dp[i][j-1]+dp[i+1][j]+1;
+                }else{
+                    dp[i][j] = dp[i][j-1]+dp[i+1][j]-dp[i+1][j-1];
+                }
+            }
+            if(j-i+1 == 5){
+                ans = max(ans, dp[i][j]);
+            }
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return ans;
+}
